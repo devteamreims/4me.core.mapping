bc80112d515c11c405ec97784f62ac20de9242cb
Benjamin Beret <benjamin.beret@gmail.com>
2016-02-25 17:54:44 +0100

    Add FOURME_DEMO env var, tweak Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,14 @@ FROM node:5.7
 RUN mkdir /usr/src/app
 WORKDIR /usr/src/app
 
-RUN mkdir /usr/src/app/db.up
-
 COPY package.json /usr/src/app
-RUN npm install
+RUN npm install --production
+
+RUN mkdir /usr/src/app/db.up
 
 COPY . /usr/src/app/
 
-EXPOSE 3000
+ENV PORT 3100
+EXPOSE ${PORT}
 
 CMD ["npm", "start"]
--- src/cwp/identifier.js
+++ src/cwp/identifier.js
@@ -6,16 +6,25 @@
  */
 
 import _ from 'lodash';
-
+import d from 'debug';
+const debug = d('4me.cwp.identifier');
 
 export function reqToCwpId(req, cwpTree) {
   if(req === undefined || req.ip === undefined || cwpTree.getAll === undefined) {
     throw new Error('Invalid argument');
   }
 
+  
   let cwp = _.find(cwpTree.getAll(), (c) => c.ipAddr.indexOf(req.ip) !== -1);
 
   if(_.isEmpty(cwp)) {
+    debug(`Got a request from an unknown IP ${req.ip}`);
+
+    if(process.env.FOURME_DEMO) {
+      const demoCwpId = 32;
+      debug(`Demo mode enabled, returning cwpId ${demoCwpId}`);
+      return demoCwpId;
+    }
     return -1;
   }
 
