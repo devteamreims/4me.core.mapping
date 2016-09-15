683a79a09a90acfd6e7bdb46fb343e187562ca49
Benjamin Beret <benjamin.beret@gmail.com>
2016-09-15 11:07:48 +0200

    Make socket.io works fine in Jest env

--- tests/testSetup.js
+++ tests/testSetup.js
@@ -1,8 +1,32 @@
 // Here we mock our database
-// We use knex and mock-knex to have the same interface
-import mockIo from './mocks/socket.io';
+// We use simple setters and getters
+jest.mock('../src/database', () => () => {
+  const api = {};
+  let cache = [];
+  const _ = require('lodash');
 
+  api.get = (key) => {
+    return Promise.resolve(cache[key]);
+  };
+
+  api.put = (key, value) => {
+    cache[key] = _.cloneDeep(value);
+    return Promise.resolve(cache[key]);
+  };
+
+  api._setCache = (newCache) => {
+    cache = _.cloneDeep(newCache);
+  };
+
+  return api;
+});
+
+
+// Mock socket.io here
 // Socket.io doesn't work in Jest environment
 // See here :
 // * https://github.com/socketio/socket.io/issues/2381
-jest.mock('socket.io', () => mockIo());
+//import mockIo from './mocks/socket.io';
+//jest.mock('socket.io', () => mockIo());
+// UPDATE : Socket.io works fine if 'jsdom' environment is set by Jest
+
