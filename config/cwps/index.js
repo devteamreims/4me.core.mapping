0c5ebc1eb02a77d949d8ee3afb2fb7825fc34975
Benjamin Beret <benjamin.beret@gmail.com>
2016-12-13 18:23:33 +0100

    Remove leftovers

--- config/cwps/index.js
+++
@@ -1,8 +0,0 @@
-let westCwp = require('./west.json');
-let eastCwp = require('./east.json');
-let nightCwp = require('./night.json');
-let specials = require('./specials.json');
-
-let RMS = westCwp.concat(eastCwp).concat(nightCwp).concat(specials);
-
-module.exports = RMS;
